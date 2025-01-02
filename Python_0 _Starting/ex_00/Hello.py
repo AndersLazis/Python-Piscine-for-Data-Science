dea193abf93f7892c8637795342431128f1c9c27
Andrey Putiev <aputiev@student.42wolfsburg.de>
2025-01-02 18:51:44 +0100

    ex00 done

--- Python_0 _Starting/ex_00/Hello.py
+++ Python_0 _Starting/ex_00/Hello.py
@@ -1,1 +1,36 @@
-w
+
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello" : "titi!"}
+
+#your code here
+
+#List:
+ft_list[-1] = "World!"
+
+#Tuple:
+tmp_list = list(ft_tuple)
+tmp_list[-1] = "Deutscland!"
+ft_tuple = tuple(tmp_list)
+
+#Set:
+ft_set.remove("tutu!")
+ft_set.add("Wolfsburg!")
+
+#Dictionary:
+ft_dict["Hello"] = "42Wolfsburg!"
+
+
+print(ft_list)
+print(ft_tuple)
+print(ft_set)
+print(ft_dict)
+
+# Expected output:
+# $>python Hello.py | cat -e
+# ['Hello', 'World!']$
+# ('Hello', 'France!')$
+# {'Hello', 'Paris!'}$
+# {'Hello': '42Paris!'}$
+# $>
